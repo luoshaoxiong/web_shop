575903b48348858755d1f708218274208ca2b8ef
luoshaoxiong <luoshaoxiong@yeah.net>
2018-10-22 21:30:33 +0800

    goodsList页面

---
+++ src/components/modal.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="mask" v-show="show">
+    <div class="modal">
+      <span class="close-btn" @click="close()">&times;</span>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,.5);
+    z-index: 9;
+  }
+
+  .modal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 535px;
+    min-height: 100px;
+    padding: 40px;
+    background-color: #fff;
+    transform: translate(-50%, -50%);
+  }
+
+  .close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    font-size: 34px;
+    color: #ccc;
+  }
+
+  .close-btn:hover {
+    color: #aaa;
+    cursor: pointer;
+  }
+</style>
--- src/views/goods-list/goods-list.vue
+++ src/views/goods-list/goods-list.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout-container">
+  <div class="layout-container clearfix">
     <div class="filter-bar">
       <span>Sort by:</span>
       <span class="option">Default</span>
@@ -15,12 +15,91 @@
         <dd>1000.00 - 2000.00</dd>
       </dl>
     </div>
+    <div class="goods-list">
+      <ul class="clearfix">
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button @click="addCart()">加入购物车</button></dd>
+          </dl>
+        </li>
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button>加入购物车</button></dd>
+          </dl>
+        </li>
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button>加入购物车</button></dd>
+          </dl>
+        </li>
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button>加入购物车</button></dd>
+          </dl>
+        </li>
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button>加入购物车</button></dd>
+          </dl>
+        </li>
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button>加入购物车</button></dd>
+          </dl>
+        </li>
+        <li>
+          <img src="/static/images/zipai.jpg">
+          <dl>
+            <dt>自拍杆</dt>
+            <dd>￥39.00</dd>
+            <dd><button>加入购物车</button></dd>
+          </dl>
+        </li>
+      </ul>
+    </div>
+    <modal :show="modalShow">
+      <div>加入购物车成功!</div>
+      <div>
+        <button>继续购物</button>
+        <button>查看购物车</button>
+      </div>
+    </modal>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import modal from '@/components/modal';
 
 export default {
+  components: {modal},
+  data () {
+    return {
+      modalShow: false
+    }
+  },
+  methods: {
+    addCart () {
+      this.modalShow = true;
+    }
+  }
 }
 
 </script>
@@ -46,7 +125,9 @@ export default {
   }
 
   .filter-aside {
+    float: left;
     width: 230px;
+    margin-right: 20px;
     padding: 0 20px;
     color: #605f5f;
     transition: right .5s ease-out;
@@ -68,4 +149,78 @@ export default {
     transition: padding-left .3s ease-out;
   }
 
+  .filter-aside dd:hover {
+    border-left: 2px solid #ee7a23;
+    padding-left: 15px;
+    cursor: pointer;
+  }
+
+  .goods-list {
+    float: left;
+    width: calc(100% - 250px);
+  }
+
+  .goods-list ul li {
+    float: left;
+    width: 24%;
+    margin-right: 1.3333%;
+    margin-bottom: 20px;
+    background: #fff;
+    border: 2px solid #e9e9e9;
+    transition: all .5s ease-out;
+  }
+
+  .goods-list ul li:hover {
+    border-color: #ee7a23;
+    transform: translateY(-5px);
+    box-shadow: 0 0 10px #999;
+  }
+
+  .goods-list ul li:nth-child(4n) {
+    margin-right: 0;
+  }
+
+  .goods-list ul li img {
+    width: 100%;
+  }
+
+  .goods-list ul li dl {
+    padding: 20px 10px 0 10px;
+  }
+
+  .goods-list ul li dt {
+    height: 4em;
+    line-height: 1.25em;
+    padding-bottom: 10px;
+    font-family: moderat,sans-serif;
+    font-weight: 700;
+    overflow: hidden;
+  }
+
+  .goods-list ul li dd {
+    margin-bottom: 10px;
+    line-height: 30px;
+    color: #d1434a;
+    font-size: 1.25em;
+  }
+
+  .goods-list ul li button {
+    display: inline-block;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    font-size: 14px;
+    font-family: moderat,sans-serif;
+    font-weight: 700;
+    border: 1px solid #d1434a;
+    color: #d1434a;
+    text-transform: uppercase;
+    letter-spacing: .25em;
+    white-space: nowrap;
+  }
+
+  .goods-list ul li button:hover {
+    background-color: #ffe5e6;
+  }
 </style>
